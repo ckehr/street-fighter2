7d23654b8930a991912904000f820ae1ace90ccf
ckehr <ckehrcmu@gmail.com>
2016-07-30 20:28:26 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    doIntro();
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+});
+$(document).keydown(function(e) {
+        if(e.which == 88) {
+        $('.ryu-still').hide();
+        $('.ryu-ready').hide();
+        $('.ryu-cool').show();
+    }
+
+
+$(document).keyup(function(e) {
+        if(e.which == 88) {
+        $('.ryu-cool').hide();  
+        $('.ryu-still').show();
+    }
+});
+});
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
+
+
+function doIntro() {
+   $('#ryu-stage')[0].volume = 0.5;
+    $('#ryu-stage')[0].load();
+    $('#ryu-stage')[0].play(); 
+}
